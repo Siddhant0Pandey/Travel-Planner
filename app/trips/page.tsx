7688ee90088682a697957ad9dbd5e3f22f2a9b06
Siddhant Pandey <pandeysiddhant0922@gmail.com>
2026-06-17 14:35:57 +0545

    button add to new trip page

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,4 +1,6 @@
 import { auth } from "@/auth"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function TripsPage() {
     const session = await auth();
@@ -9,5 +11,15 @@ return(
     </div>
 )
     }
+
+    return(
+        <div className="max-w-2xl mx-auto mt-10">
+            <h1>Welcome to Your Trips</h1>
+            <Link href='/trips/new'>
+            
+            <Button>Add Trip</Button>
+            </Link>
+        </div>
+    )
    
 }
